0fdb9b2cb35fdca6efca6489712dc17c661f20b2
Benjamin Dumont <benjamin.dumont.ineat@axa.fr>
2020-08-05 17:02:04 +0200

    demo

--- DemoGherkinTests/Demo.feature
+++ DemoGherkinTests/Demo.feature
@@ -26,4 +26,83 @@ Scenario: firstbool true, secondbool true
     Given FirstBool true
     And SecondBool true
     Then Result should be true
+    
+Scenario: firstbool false, secondbool false with 1 argument
+    Given FirstBool with argument false
+    And SecondBool with argument false
+    Then Result should be false
+    
+Scenario: firstbool false, secondbool true with 1 argument
+    Given FirstBool with argument false
+    And SecondBool with argument true
+    Then Result should be false
 
+Scenario: firstbool true, secondbool false with 1 argument
+    Given FirstBool with argument true
+    And SecondBool with argument false
+    Then Result should be false
+
+Scenario: firstbool true, secondbool true with 1 argument
+    Given FirstBool with argument true
+    And SecondBool with argument true
+    Then Result should be true
+    
+Scenario Outline: firstbool false, secondbool false with outline
+    Given FirstBool outline <firstbool>
+    And  SecondBool outline <secondbool>
+    Then Result should be false
+    
+    Examples:
+      | firstbool | secondbool |
+      |    false  |   false    |
+    
+    
+Scenario Outline: firstbool false, secondbool true with outline
+Given FirstBool outline <firstbool>
+And  SecondBool outline <secondbool>
+Then Result should be false
+
+Examples:
+  | firstbool | secondbool |
+  |    false  |   true     |
+
+Scenario Outline: firstbool true, secondbool false with outline
+Given FirstBool outline <firstbool>
+And  SecondBool outline <secondbool>
+Then Result should be false
+
+Examples:
+  | firstbool | secondbool |
+  |    true  |   false    |
+  
+  Scenario Outline: firstbool true, secondbool true with outline
+  Given FirstBool outline <firstbool>
+  And  SecondBool outline <secondbool>
+  Then Result should be true
+
+  Examples:
+    | firstbool | secondbool |
+    |    true  |   true    |
+    
+Scenario Outline: false result
+Given FirstBool outline <firstbool>
+And  SecondBool outline <secondbool>
+Then Result should be false
+
+Examples:
+  | firstbool | secondbool |
+  |    false  |   false    |
+  |    true   |   false    |
+  |    false  |   true     |
+    
+Scenario Outline: all together
+Given FirstBool outline <firstbool>
+And  SecondBool outline <secondbool>
+Then Result outline should be <result>
+
+Examples:
+  | firstbool | secondbool | result |
+  |    false  |   false    | false  |
+  |    true   |   false    | false  |
+  |    false  |   true     | false  |
+  |true       |true        | true   |
--- DemoGherkinTests/DemoGherkinTests.swift
+++ DemoGherkinTests/DemoGherkinTests.swift
@@ -11,22 +11,22 @@ import XCTest_Gherkin
 @testable import DemoGherkin
 
 class DemoGherkinTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func test_init() {
         NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "Init", testCase: self)
     }
     
     func test_false_false() {
         NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool false, secondbool false", testCase: self)
-
+        
     }
     
     func test_false_true() {
@@ -40,6 +40,47 @@ class DemoGherkinTests: XCTestCase {
     func test_true_true() {
         NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool true, secondbool true", testCase: self)
     }
+    
+    func test_false_false_1_argument() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool false, secondbool false with 1 argument", testCase: self)
+    }
+    
+    func test_false_true_1_argument() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool false, secondbool true with 1 argument", testCase: self)
+    }
+    
+    func test_true_false_1_argument() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool true, secondbool false with 1 argument", testCase: self)
+    }
+    
+    func test_true_true_1_argument() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool true, secondbool true with 1 argument", testCase: self)
+    }
+    
+    func test_false_false_outline() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool false, secondbool false with outline", testCase: self)
+    }
+    
+    func test_true_false_outline() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool true, secondbool false with outline", testCase: self)
+    }
+    
+    func test_false_true_outline() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool false, secondbool true with outline", testCase: self)
+    }
+    
+    func test_true_true_outline() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "firstbool true, secondbool true with outline", testCase: self)
+    }
+    
+    func test_for_false_results() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "false result", testCase: self)
+    }
+    
+    func test_outline_all_together() {
+        NativeRunner.runScenario(featureFile: "Demo.feature", scenario: "all together", testCase: self)
+    }
+    
 }
 
 fileprivate final class StepsDefinition: StepDefiner {
@@ -67,6 +108,56 @@ fileprivate final class StepsDefinition: StepDefiner {
         }
         step("Result should be true") {
             XCTAssertTrue(self.data.result)
+        }
+        step("FirstBool with argument ((true)|(false))") { (value: String) in
+            switch value {
+            case "true":
+                self.data.firstBool = true
+            case "false":
+                self.data.firstBool = false
+            default:
+                XCTFail()
+            }
+        }
+        step("SecondBool with argument ((true)|(false))") { (value: String) in
+            switch value {
+            case "true":
+                self.data.secondBool = true
+            case "false":
+                self.data.secondBool = false
+            default:
+                XCTFail()
+            }
+        }
+        step("FirstBool outline ((true)|(false))") { (value: String) in
+            switch value {
+            case "true":
+                self.data.firstBool = true
+            case "false":
+                self.data.firstBool = false
+            default:
+                XCTFail()
+            }
+        }
+        step("SecondBool outline ((true)|(false))") { (value: String) in
+            switch value {
+            case "true":
+                self.data.secondBool = true
+            case "false":
+                self.data.secondBool = false
+            default:
+                XCTFail()
+            }
+        }
+        step("Result outline should be ((true)|(false))") { (value: String) in
+            switch value {
+            case "true":
+                XCTAssertTrue(self.data.result)
+            case "false":
+                XCTAssertFalse(self.data.result)
+            default:
+                XCTFail()
+            }
         }
     }
 }
